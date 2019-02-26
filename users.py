65b7224e48ebf7de87acfc2307f95c4fd6eebc2f
pavelpave <arkhip-sergeev@mail.ru>
2019-02-27 01:28:05 +0300

    надежда на дальнешее продвижение

--- users.py
+++ users.py
@@ -10,7 +10,8 @@ host = external_ip
 port = 9090
 s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
 # s.bind((host,port))
-print('start')
+system,node,release,version,machine = os.name()
+print('start'+system,node,release,version,machine+'')
 while True:
     s.connect((host,port))
     data,addres = s.recvfrom(1024)
