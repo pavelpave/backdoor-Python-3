ca8bd1841b3231e8e733503eeb0620624bbac16a
pavelpave <arkhip-sergeev@mail.ru>
2019-04-07 13:18:09 +0400

    Вспоминаем про питон

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
 import socket
+import urllib.request
 
-host = "192.168.43.22"
+host = urllib.request.urlopen('http://ident.me').read().decode("utf-8")
+#host = "192.168.43.22"
+#212.26.236.115
+
+print(host)
 port = 9090
 s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
-s.bind((host,port))
+s.bind(("127.0.0.1",9090))
 print("start_server  "+ socket.gethostname() +"")
-server = (9090,host)
+server = (host,9090)
 massAddr = []
 
 while True :
     data,addres = s.recvfrom(1024)
-    data.decode()
+    data.decode("utf-8")
+    print(data)
 
     if not addres in massAddr:
         massAddr.append(addres)
--- users.py
+++ users.py
@@ -1,6 +1,7 @@
 import socket
 import os
 import urllib.request
+import subprocess
 
 external_ip = urllib.request.urlopen('http://ident.me').read().decode('utf8')
 
@@ -8,12 +9,19 @@ print(external_ip)
 
 host = external_ip
 port = 9090
+
+server = ("212.26.236.115",9090)
 s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
 # s.bind((host,port))
-system,node,release,version,machine = os.name()
-print('start'+system,node,release,version,machine+'')
-while True:
-    s.connect((host,port))
-    data,addres = s.recvfrom(1024)
-    udata = data.decode("utf-8")
-    print(udata)
+print('start')
+try:
+    while True:
+
+        s.connect(server)
+        data,addres = s.recvfrom(1024)
+        udata = data.decode("utf-8")
+        print(data)
+        s.send(b"hi")
+
+except:
+    raise("out")
